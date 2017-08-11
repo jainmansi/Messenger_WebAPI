a0066ba28027c40c0d4cdb4389d7420e23193e0f
Mansi Jain <mansijain.nitrr@gmail.com>
2017-08-10 23:28:04 -0400

    Adding pagination and filtering of data

--- src/main/java/org/firstRESTapi/messenger/resources/MessageResource.java
+++ src/main/java/org/firstRESTapi/messenger/resources/MessageResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.firstRESTapi.messenger.model.Message;
@@ -21,30 +22,39 @@ import org.firstRESTapi.messenger.service.MessageService;
 public class MessageResource {
 
 	MessageService messageService = new MessageService();
-	
+
 	@GET
-	public List<Message> getMessages() {
+	public List<Message> getMessages(@QueryParam("year") int year, 
+									@QueryParam("start") int start,
+									@QueryParam("size") int size) {
+		if (year > 0) {
+			return messageService.getAllMessagesForYear(year);
+		}
+		
+		if(start >= 0 && size > 0) {
+			return messageService.getAllMessagesPaginated(start, size);
+		}
 		return messageService.getAllMessages();
 	}
-	
+
 	@POST
 	public Message addMessages(Message message) {
 		return messageService.addMessage(message);
 	}
-	
+
 	@PUT
 	@Path("/{messageId}")
 	public Message updateMessage(@PathParam("messageId") long id, Message message) {
 		message.setId(id);
 		return messageService.updateMessage(message);
 	}
-	
+
 	@DELETE
 	@Path("/{messageId}")
 	public void removeMessage(@PathParam("messageId") long id) {
 		messageService.removeMessage(id);
 	}
-	
+
 	@GET
 	@Path("/{messageId}")
 	public Message getMessage(@PathParam("messageId") long id) {
--- src/main/java/org/firstRESTapi/messenger/service/MessageService.java
+++ src/main/java/org/firstRESTapi/messenger/service/MessageService.java
@@ -1,6 +1,7 @@
 package org.firstRESTapi.messenger.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -40,6 +41,26 @@ public class MessageService {
 	
 	public Message removeMessage(Long id) {
 		return messages.remove(id);
+	}
+	
+	public List<Message> getAllMessagesForYear(int year) {
+		List<Message> messageForYear = new ArrayList<>();
+		Calendar cal = Calendar.getInstance();
+		for(Message message : messages.values()) {
+			cal.setTime(message.getCreated());
+			if (cal.get(Calendar.YEAR) == year) {
+				messageForYear.add(message);
+			}
+		}
+		return messageForYear;
+	}
+	
+	public List<Message> getAllMessagesPaginated(int start, int size) {
+		ArrayList<Message> allMessages = new ArrayList<>(messages.values());
+		if (start + size > allMessages.size()) {
+			return new ArrayList<>();
+		}
+		return allMessages.subList(start, start + size);
 	}
 	
 }
